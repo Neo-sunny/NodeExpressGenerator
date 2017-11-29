541b61b48c4130d797c7795d4275dd2f49d03724
neo <neo.sunny2k13@gmail.com>
2017-11-29 15:26:14 +0530

    Using the Session to authenticate and track the user

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -39,9 +41,59 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+// Instead of cookie parser we will use session
+//app.use(cookieParser('12345-67890-09876-54321'));
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
+
+function auth(req, res, next){
+  console.log(req.session);
+
+if(!req.session.user){ // If request does not contains signedCookies
+
+  // then go for Basic authentication
+    var authHeader= req.headers.authorization;
+    if(!authHeader){
+      var err=new Error("You are not authenticated ");
+      err.status=401;
+      res.setHeader('WWW-Authenticate', 'Basic');
+      return next(err);
+    }
+
+    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+
+    var userName= auth[0];
+    var pass= auth[1];
+    if(userName== 'admin' && pass== 'password'){
+      req.session.user= 'admin';
+      next();
+    }else{
+      err = new Error("You are not authenticated ");
+      err.status=401;
+      res.setHeader('WWW-Authenticate', 'Basic');
+      return next(err);
+    }
+  }else{
+    if(req.session.user === 'admin'){
+      next();
+    }else{
+      var err=new Error("You are not authenticated ");
+      err.status=401;
+      return next(err);
+    }
+  }
+  
+}
 
 
+
+
+/* Using Cookies with Basic Authentication
 function auth(req, res, next){
   console.log(req.signedCookies);
 
@@ -80,12 +132,9 @@ if(!req.signedCookies.user){ // If request does not contains signedCookies
   }
   
 }
+*/
 
-
-
-
-
-/*
+/*  Using Basic Authorization
 function auth(req, res, next){
   console.log(req.headers);
   var authHeader= req.headers.authorization;
